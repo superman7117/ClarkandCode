a50c9ccf625ab963662a88b460d38396274829ba
Clark Ozanne <clarkozanne@gmail.com>
2016-02-25 09:06:04 -0800

    added contact me element and code element

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 var theApp = angular.module("theApp", ['ui.router'])
 
-theApp.config(function($stateProvider, $urlRouteProvider){
+theApp.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
   .state('/', {url: '/', templateUrl: 'partials/index.html'})
-  .state('/', {url: '/clark', templateUrl: 'partials/clark.html'})
-  .state('/', {url: '/code', templateUrl: 'partials/code.html'})
-  .state('/', {url: '/contact', templateUrl: 'partials/contact.html'})
+  .state('clark', {url: '/clark', templateUrl: 'partials/clark.html'})
+  .state('code', {url: '/code', templateUrl: 'partials/code.html'})
+  .state('contact', {url: '/contact', templateUrl: 'partials/contact.html'})
 
-  $urlRouteProvider.otherwise("/")
+  $urlRouterProvider.otherwise("/")
 })
